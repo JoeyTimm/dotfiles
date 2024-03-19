ce2e192de2985902d3ff96da5981fbd9adb2b1d8
JoeyTimm <timm.3@wright.edu>
2024-03-19 17:32:54 -0400

    done with anaconda, awscli, and install.sh

---
+++ .bashrc
@@ -0,0 +1,19 @@
+alias :q="exit"
+#I use vim so I am used to doing :q to quit so this just appeals to my muscle memor from vim and lets me use :q for everything
+
+alias sai="sudo apt install"
+alias sai="sudo apt-get install"
+#I find that following along with class demos and guides results in me downloading a lot of extensions so this saves me some typing
+
+alias l='ls -lah'
+#ls -lah is probably my most used command so why not make my life easier
+
+#something from the ultimate BA bashrc file
+# Color for manpages in less makes manpages a little easier to read
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#based on script by professor
+
 #get username that called script
 echo $SUDO_USER
 ME=$SUDO_USER
@@ -13,31 +15,10 @@ if [[ "$(id -u)" -eq 0 ]]; then
 		echo "apt is installed exactly as specified."
 		apt install -y \
 			nmap \
-			dnsutils \
-			libgl1-mesa-glx \
-			libegl1-mesa \
-			libxrandr2 \
-			libxss1 \
-			libxcursor1 \
-			libxcomposite1 \
-			libasound2 \
-			libxi6 \
-			libxtst6
-		if [[ -e $(ls Anaconda3*.sh 2> /dev/null | head -1) ]]; then
-			echo "Installer found, running it"
-			bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
-			echo "Path=$PATH:/anaconda3/bin" >> /home/$ME/.profile
-		else
-			echo "Downloading anaconda installer"
-			curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
-			bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
-			echo "PATH=$PATH:/anaconda3/bin" >> /home/$ME/.profile
-		fi	
+			dnsutils \	
 	else
 		echo "apt is not installed at the specified location."
 	fi
-	
-	# install packages with apt
 else
 	echo "Script is not running as root, exiting..." 1>&2
 	exit 1
