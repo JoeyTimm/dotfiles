cf0a516dc3542d24dad861381c9fd0596aeefa1f
JoeyTimm <timm.3@wright.edu>
2024-03-19 17:53:39 -0400

    vundle install complete

---
+++ vundle-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#install from professor example
+
+mkdir -p ~/.vim/bundle
+#Clone Vundle repository
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#create backup of current .vimrc file
+if [ -f ~/.vimrc ]; then
+	    mv ~/.vimrc ~/.vimrc.backup
+fi
+echo "Vundle has been successfully installed"
+echo "Removed Vundle default vimrc"
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+echo "Replaced with repo vimrc"
+
+vim +PluginInstall +qall
+echo "Installed plugin list in vim"
+
+#for ycm
+python3 ~/.vim/bundle/youcompleteme/install.py --all
